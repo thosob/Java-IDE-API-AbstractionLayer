0e7fd5ba2713a63777911de64a63139a4e1f1682
thoma <thoma@DESKTOP-U6N9G3D.SpeedportEntry2090126200030>
2017-05-17 15:55:52 +0200

    fixed bug, that annotation weren't used in netbeans

---
+++ Application/src/de/uos/application/Main.java
@@ -0,0 +1,17 @@
+package de.uos.application;
+
+/**
+ * @brief Start method
+ * @author Thomas Sobieroy
+ */
+public class Main {
+    /**
+     * @brief starting
+     * @param args
+     * @param argv 
+     */
+    public static void Main(String[] args, int argv){
+        System.out.println("test");
+    }
+}
+
